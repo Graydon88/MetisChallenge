70982b7fc0a3775b9df69f91ed64266749708501
Graydon88 <graydon.giles@gmail.com>
2022-10-29 12:39:21 -0600

    basic outline of function before implementing logic

--- phone-number.js
+++ phone-number.js
@@ -1,4 +1,12 @@
+// Take input and remove non-digit characters then process the remaining data for 10 digit cleansed output
 
-export const clean = () => {
-  throw new Error("Remove this statement and implement this function");
+export const clean = (phoneNbr) => {
+  var cleanNumber = phoneNbr.replace(/[^\d]/g, '');
+
+  if(cleanNumber.length != 10) {
+    //Evaluate for number greater than 10 or smaller than 10
+  } else {
+    //Evaluate the initial digit to verify it is not a 0 or 1
+  }
+
 };
